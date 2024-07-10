feb71eb32077ec1b7beec8b530796c312e200677
04samarthg <04samarthgupta@gmail.com>
2024-07-10 18:32:55 +0530

    tweaks

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,6 @@
 return {
   "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "xiyaowong/transparent.nvim" },
   version = "*",
   opts = {
     options = {
@@ -8,4 +8,5 @@ return {
       separator_style = "slant",
     },
   },
+  require('transparent').clear_prefix('BufferLine')
 }
