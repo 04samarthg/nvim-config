9acb99ef81e122992f8f6f5da5b3535b5b10a277
04samarthg <04samarthgupta@gmail.com>
2025-01-12 00:20:48 +0530

    smear animation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,7 +8,14 @@ return {
     'HiPhish/rainbow-delimiters.nvim',
     event = "BufReadPost",
   },
-  "sphamba/smear-cursor.nvim",
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      smear_between_buffers = true,
+      smear_between_neighbor_lines = true,
+      scroll_buffer_space = true,
+    },
+  },
   {
     "stevearc/dressing.nvim",
     event = "VeryLazy",
